d41a12eeac5328a480efc321ac7009360f8ae547
Ethanjonm <ethanjonmercado@gmail.com>
2023-04-05 09:49:18 -0400

    created table still some errors

--- import_db.sql
+++ import_db.sql
@@ -1,3 +1,6 @@
+PRAGMA foreign_keys = ON;
+
+
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
@@ -34,3 +37,30 @@ CREATE TABLE replies (
     FOREIGN KEY (reply_id) REFERENCES replies(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+CREATE TABLE question_likes (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+
+INSERT INTO 
+    users (fname, lname)
+VALUES
+    ('John', 'Doe'),
+    ('Snow', 'White');
+
+INSERT INTO
+    questions (title, body, author_id)
+VALUES
+    ('Why', 'Why is here hair blue', (SELECT id FROM users WHERE fname = 'John')),
+    ('Who', 'Who is Snow White', (SELECT id FROM users WHERE fname = 'Snow' AND lname = 'White'));
+
+-- INSERT INTO 
+--     replies (question_id, reply_id, user_id, body)
+-- VALUES
+--     (1, NULL, 2, 'Great question');
