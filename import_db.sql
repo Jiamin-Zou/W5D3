6aca8813960d5650b85aab9907ef43d0e52f8f4d
Jiamin-Zou <jiaminz622@gmail.com>
2023-04-05 10:08:41 -0400

    Completed table creation

--- import_db.sql
+++ import_db.sql
@@ -1,21 +1,10 @@
 PRAGMA foreign_keys = ON;
 
-
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY,
-    fname TEXT NOT NULL,
-    lname TEXT NOT NULL
-
-);
-
-CREATE TABLE questions (
-    id INTEGER PRIMARY KEY,
-    title TEXT NOT NULL,
-    body TEXT NOT NULL
-    author_id INTEGER,
-
-    FOREIGN KEY (author_id) REFERENCES users(id)
-);
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE question_follows (
     id INTEGER PRIMARY KEY,
@@ -47,6 +36,20 @@ CREATE TABLE question_likes (
     FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    author_id INTEGER NOT NULL,
+
+    FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL
+);
 
 INSERT INTO 
     users (fname, lname)
@@ -60,7 +63,7 @@ VALUES
     ('Why', 'Why is here hair blue', (SELECT id FROM users WHERE fname = 'John')),
     ('Who', 'Who is Snow White', (SELECT id FROM users WHERE fname = 'Snow' AND lname = 'White'));
 
--- INSERT INTO 
---     replies (question_id, reply_id, user_id, body)
--- VALUES
---     (1, NULL, 2, 'Great question');
+INSERT INTO 
+    replies (question_id, reply_id, user_id, body)
+VALUES
+    (1, NULL, 2, 'Great question');
